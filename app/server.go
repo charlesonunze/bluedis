3c30cc0606b2d8f5ce4de07279a83b2043196493
charlesonunze <charlesonunze2@gmail.com>
2023-03-24 13:48:02 +0100

    feat: respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -9,25 +9,33 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379", err)
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		defer conn.Close()
 
+		go readLoop(conn)
+	}
+}
+
+func readLoop(conn net.Conn) {
 	buf := make([]byte, 1024)
 
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		if _, err := conn.Read(buf); err != nil {
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
 
-	conn.Write([]byte("+PONG\r\n"))
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
